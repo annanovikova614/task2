cac515a65debe7c467c566f38802b8de199c8d2b
annanovikova614 <72442702+annanovikova614@users.noreply.github.com>
2020-10-27 15:37:15 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,32 @@
-package com.company;
+package com.company; 
+ 
+import java.util.Scanner; 
+ 
+public class Main { 
 
-import java.util.Scanner;
+    public static void main(String[] args) { 
+        int year = readYear("год"); 
+        int result = calculateCountOfDaysInYear(year); 
+        printCountOfDaysInYear(result); 
+        writeLeapOrNonLeapYear(result); 
+    } 
+ 
+    private static int readYear(String string) { 
+        Scanner scanner = new Scanner(System.in); 
+        System.out.printf("Введите %s: ", string); 
+        return scanner.nextInt(); 
+    } 
 
-public class Main {
+    private static void printCountOfDaysInYear(int result) { 
+        System.out.printf("Количество дней в году %d\n", result); 
+    } 
 
-    public static void main(String[] args) {
-        int year = readYear("год");
-        int result = calculateCountOfDaysInYear(year);
-        printCountOfDaysInYear(result);
-    }
+    private static void writeLeapOrNonLeapYear(int result) {
+        System.out.println("Год " + ((result == 365) ? "не" : "") + "високосный");
+    } 
 
-    private static int readYear(String string) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.printf("Введите %s:", string);
-        return scanner.nextInt();
-    }
-
-    private static void printCountOfDaysInYear(int result) {
-        System.out.printf("Колличество дней в году %d", result);
-    }
-
-    public static int calculateCountOfDaysInYear(int year) {
-        return ((year % 4 != 0) || (year % 100 == 0 && year % 400 != 0)) ? 365 : 366;
-    }
+    private static int calculateCountOfDaysInYear(int year) { 
+        return ((year % 4 != 0) || (year % 100 == 0 && year % 400 != 0)) ? 365 : 366; 
+    } 
 }
+
